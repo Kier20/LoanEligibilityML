cf79937cc4d162d6d44093daba60769d4b6e20e5
Kier20 <118508879+Kier20@users.noreply.github.com>
2025-07-10 16:36:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -187,13 +187,11 @@ def predict():
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))  # Render sets the PORT env variable
-    app.run(host="0.0.0.0", port=port)
-    
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render sets the PORT env variable
+    app.run(host="0.0.0.0", port=port)
